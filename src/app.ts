a4bbe017ff40aa788a02c4e198bf7539feda21ae
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-09-07 12:21:04 +0600

    renovate.json

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import person from "./routes/person";
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-app.use("/p", person);
+app.use("/person", person);
 
 app.listen(app.get("port"), () => {
     console.log("App is running at http://localhost:%d", app.get("port"));
